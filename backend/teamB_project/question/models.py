d1d73d235d008f5c19aa9798901275f203137f50
서보성 <sbosung0610@gmail.com>
2025-06-29 21:44:08 +0900

    bosungUpdate

---
+++ backend/teamB_project/question/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from main.models import User
+# from . import Category
+# 실제 카테고리가 어디에 정의되는지에 따라 다름
+
+# Create your models here.
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question')
+    # category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='question')
+    # 외래키
+
+    title = models.CharField(max_length=500)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'question' # DB 테이블 명
+        
