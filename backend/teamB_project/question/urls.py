3ef4d11b15d303b199228d37f851a03202348179
이채영 <lohoho619@inha.edu>
2025-06-30 20:28:31 +0900

    feature: ranking 기능

--- backend/teamB_project/question/urls.py
+++ backend/teamB_project/question/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 # from .views import CategoryListCreateView, CategoryDetailView
 
+# ranking
+from .views import QuestionRankingView
+
 urlpatterns = [
-    
+    path('ranking/', QuestionRankingView.as_view()),
 ]
