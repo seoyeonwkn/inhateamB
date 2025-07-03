970ab60a83bf2e49c1a23da84dc29baf75d9ad7f
이채영 <lohoho619@inha.edu>
2025-07-03 14:12:49 +0900

    BadgeLevel 제거, badge CRUD 수정 완료

--- backend/teamB_project/badge/urls.py
+++ backend/teamB_project/badge/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from .views import BadgeListCreateView
-from .views import UserBadgeListView
-from .views import RecommendedBadgeView
-from .views import AwardBadgeView
-from .views import UserBadgeListView
+from .views import (
+    BadgeListCreateView,
+    BadgeDetailView,
+    AwardBadgeView,
+    UserBadgeListView,
+    RecommendedBadgeView,
+)
 
 urlpatterns = [
-    path('',                  BadgeListCreateView.as_view()),         # [GET] 전체 뱃지 조회  [POST] 뱃지 생성
-#    path('me/',               UserBadgeListView.as_view()),           # [GET] (임시) user-id 없이 자신의 뱃지 조회
-#    path('recommend/',        RecommendedBadgeView.as_view()),        # [GET] 추천 뱃지 3개
-    path('award/<int:user_id>/',  AwardBadgeView.as_view()),          # [POST] 토큰 없이 뱃지 지급
-    path('recommend/<int:user_id>/', RecommendedBadgeView.as_view()), # [GET] 토큰 없이 추천 뱃지 3개
-    path('user/<int:user_id>/',   UserBadgeListView.as_view()),       # [GET] 토큰 없이 특정 유저의 뱃지 조회
+    path('', BadgeListCreateView.as_view()),                   # [GET, POST] 전체 조회 / 생성
+    path('<int:pk>/', BadgeDetailView.as_view()),              # [GET, PUT, DELETE] 상세 조회/수정/삭제
+    path('award/<int:user_id>/', AwardBadgeView.as_view()),    # [POST] 뱃지 지급
+    path('user/<int:user_id>/', UserBadgeListView.as_view()),  # [GET] 유저의 뱃지 목록
+    path('recommend/<int:user_id>/', RecommendedBadgeView.as_view()),  # [GET] 미보유 랜덤 추천
 ]
-
-
